168213d1c28de6cb585f76d618748b12432b6255
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-03-19 17:22:12 +0530

    Add config to use serverless only on Now

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  target: "serverless"
-};
+const config = {};
+
+if (process.env.NOW_REGION || process.env.NOW) {
+  config.target = "serverless";
+}
+
+module.exports = config;
