61f2b7984c56cd2b1822b02deaac263b914b68fc
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-03-20 18:02:36 +0530

    Fix test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,6 +3,6 @@
 describe("My First Test", function() {
   it("Visits the Kitchen Sink", function() {
     cy.visit("http://localhost:3000");
-    cy.contains("Hello, world!");
+    cy.contains("Hello, Wikipedia!");
   });
 });
