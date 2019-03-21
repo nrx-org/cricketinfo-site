9576aa1f422227a1d9e0074908d98c7d37919c9b
Ankur Sethi <contact@ankursethi.in>
2019-03-21 15:47:29 +0530

    Fetch article from Wikipedia and display it

--- pages/article.js
+++ pages/article.js
@@ -1,18 +1,24 @@
 import React from "react";
+import wtf from "wtf_wikipedia";
 
-function Article({ articleId, lang }) {
+function Article({ title, content }) {
   return (
     <div>
-      <h1>{ articleId }</h1>
-      <h1>{ lang }</h1>
+      <h1>{ title }</h1>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
     </div>
   );
 }
 
 Article.getInitialProps = async ({ query }) => {
+  const { articleId, lang } = query;
+  const article = await wtf.fetch(articleId, lang);
+
   return {
-    articleId: query.articleId,
-    lang: query.lang
+    articleId,
+    lang,
+    content: article.html(),
+    title: article.title()
   };
 };
 
