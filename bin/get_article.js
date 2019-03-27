3e6e26c320c6e2bff1642ac573589f41f1cc1c81
Ankur Sethi <contact@ankursethi.in>
2019-03-27 15:15:48 +0530

    Add a script to download and clean up articles

---
+++ bin/get_article.js
@@ -0,0 +1,72 @@
+const fetch = require("isomorphic-fetch");
+const utf8 = require("utf8");
+const cheerio = require("cheerio");
+
+const SECTION_TITLES = {
+  en: {
+    externalLinks: "External Links",
+    seeAlso: "See Also",
+    references: "References"
+  },
+  hi: {
+    externalLinks: "बाहरी कड़ियाँ",
+    seeAlso: "इन्हें भी देखें",
+    references: "सन्दर्भ"
+  }
+};
+
+function stripSelectors(html, selectorsList) {
+  const $ = cheerio.load(html, { decodeEntities: false });
+  selectorsList.forEach(s => $(s).remove());
+  return $("body").html();
+}
+
+function stripCommonElements(html) {
+  return stripSelectors(html, [
+    ".infobox",
+    ".mw-ref",
+    ".reference",
+    ".reflist",
+    ".mbox-small",
+    "figure"
+  ]);
+}
+
+function filterCommonSections(sections, lang) {
+  return sections.filter(
+    s => !Object.values(SECTION_TITLES[lang]).includes(s.line)
+  );
+}
+
+async function getArticle(articleId, lang) {
+  const url = utf8.encode(
+    `https://${lang}.wikipedia.org/api/rest_v1/page/mobile-sections/${articleId}`
+  );
+  const articleResponse = await fetch(url);
+  const articleJson = await articleResponse.json();
+  // console.log(articleJson.lead.sections[0]);
+
+  const sanitizedArticle = {
+    coverImage: "{{ TODO }}"
+  };
+
+  // Get summary (i.e the first section). Remove infobox.
+  sanitizedArticle.summary = stripCommonElements(
+    articleJson.lead.sections[0].text
+  );
+
+  // Get the remaining sections. Remove infoboxes and images if they exist.
+  sanitizedArticle.sections = filterCommonSections(
+    articleJson.remaining.sections,
+    lang
+  ).map(section => ({
+    title: section.line,
+    anchor: section.anchor,
+    content: stripCommonElements(section.text),
+    tocLevel: section.toclevel
+  }));
+
+  console.log(JSON.stringify(sanitizedArticle));
+}
+
+getArticle("मंगलयान", "hi");
