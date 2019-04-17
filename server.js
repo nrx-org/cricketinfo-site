a5488af3c36cdf6057ecaf8f33d4f0e93451c143
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-04-17 14:59:53 +0530

    Revert experiment https on localhost

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-// const express = require("express");
+const express = require("express");
 const next = require("next");
-const https = require("https");
-const fs = require("fs");
-const { parse } = require("url");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -11,44 +8,28 @@ const handle = app.getRequestHandler();
 app
   .prepare()
   .then(() => {
-    // const server = express();
+    const server = express();
 
-    // server.get("/wiki/:lang/:articleId", (req, res) => {
-    //   const actualPage = "/article";
+    server.get("/wiki/:lang/:articleId", (req, res) => {
+      const actualPage = "/article";
 
-    //   const queryParams = {
-    //     articleId: req.params.articleId,
-    //     lang: req.params.lang
-    //   };
+      const queryParams = {
+        articleId: req.params.articleId,
+        lang: req.params.lang
+      };
 
-    //   app.render(req, res, actualPage, queryParams);
-    // });
+      app.render(req, res, actualPage, queryParams);
+    });
 
-    // server.get("*", (req, res) => {
-    //   return handle(req, res);
-    // });
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
-    https
-      .createServer(
-        {
-          key: fs.readFileSync("local-cert-generator/server.key"),
-          cert: fs.readFileSync("local-cert-generator/server.crt")
-        },
-        (req, res) => {
-          const parsedUrl = parse(req.url, true);
-          handle(req, res, parsedUrl);
-        }
-      )
-      .listen(3000, err => {
-        if (err) throw err;
-        console.log("Listening with https on 443");
-      });
-
-    // server.listen(3000, err => {
-    //   if (err) throw err;
-    //   // eslint-disable-next-line no-console
-    //   console.log("> Ready on http://localhost:3000/");
-    // });
+    server.listen(3000, err => {
+      if (err) throw err;
+      // eslint-disable-next-line no-console
+      console.log("> Ready on http://localhost:3000/");
+    });
   })
   .catch(err => {
     // eslint-disable-next-line no-console
