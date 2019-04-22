f20a7836d0fd835457ecdc4492f96638522d301b
Ankur Sethi <contact@ankursethi.in>
2019-04-22 18:14:15 +0530

    Add multiple sizes to the bottom sheet

--- components/BottomSheet.jsx
+++ components/BottomSheet.jsx
@@ -4,26 +4,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const BottomSheet = ({ isOpen, onOverlayClick, children }) => {
+export const BottomSheet = ({ isOpen, onOverlayClick, size, children }) => {
   const openClassName = isOpen ? "wcp-bottom-sheet--open" : "";
+  const sizeClassName = `wcp-bottom-sheet--${size}`;
+
   return (
     <>
       <div
         className={`wcp-bottom-sheet__overlay ${openClassName}`}
         onClick={onOverlayClick}
       />
-      <div className={`wcp-bottom-sheet ${openClassName}`}>{children}</div>
+      <div className={`wcp-bottom-sheet ${sizeClassName} ${openClassName}`}>
+        {children}
+      </div>
     </>
   );
 };
 
 BottomSheet.defaultProps = {
   isOpen: false,
-  onOverlayClick: () => ({})
+  onOverlayClick: () => ({}),
+  size: "3up"
 };
 
 BottomSheet.propTypes = {
   isOpen: PropTypes.bool,
   children: PropTypes.node.isRequired,
-  onOverlayClick: PropTypes.func
+  onOverlayClick: PropTypes.func,
+  size: PropTypes.oneOf(["1up", "2up", "3up", "4up"])
 };
