efeae34f6be1d680c787af244245b4ef5c461b81
Meet <meetbhatt23@gmail.com>
2019-04-22 16:09:30 +0530

    Vertical Timeline component ready for review

--- components/FactCard.jsx
+++ components/FactCard.jsx
@@ -12,23 +12,24 @@ import {
   SHARE_FACT_CARD_BOTTOM_SHEET_ID
 } from "../lib/modal_ids";
 import { getImageShareUrl } from "../lib/urls";
+import {VerticalTimelineContent} from "./VerticalTimelineContent"
 
 const TableContent = ({ cardData }) => {
   const content = cardData.facts.map(f => (
     <tr key={f.label}>
-      <th>{f.label}</th>
-      <td>
-        {f.value.url ? (
-          <a href={f.value.url}>{f.value.label}</a>
-        ) : (
-          f.value.label
-        )}
-      </td>
+    <th>{f.label}</th>
+    <td>
+    {f.value.url ? (
+      <a href={f.value.url}>{f.value.label}</a>
+    ) : (
+      f.value.label
+    )}
+    </td>
     </tr>
   ));
   return (
     <table className="wcp-fact-card__table-content ">
-      <tbody>{content}</tbody>
+    <tbody>{content}</tbody>
     </table>
   );
 };
@@ -39,46 +40,46 @@ TableContent.propTypes = {
 
 const AvatarContent = ({ cardData }) => (
   <div className="wcp-fact-card__avatar-content">
-    {cardData.facts.map(f => {
-      const content = (
-        <div className="wcp-fact-card__avatar-content__item">
-          <div className="wcp-fact-card__avatar-content__item__profile-picture">
-            <img src={f.value.image.url} alt={f.value.image.alt} />
-          </div>
-          <div className="wcp-fact-card__avatar-content__item__info">
-            <div className="wcp-fact-card__avatar-content__item__info__label">
-              {f.label}
-            </div>
-            <div className="wcp-fact-card__avatar-content__item__info__value">
-              {f.value.label}
-            </div>
-          </div>
-        </div>
-      );
+  {cardData.facts.map(f => {
+    const content = (
+      <div className="wcp-fact-card__avatar-content__item">
+      <div className="wcp-fact-card__avatar-content__item__profile-picture">
+      <img src={f.value.image.url} alt={f.value.image.alt} />
+      </div>
+      <div className="wcp-fact-card__avatar-content__item__info">
+      <div className="wcp-fact-card__avatar-content__item__info__label">
+      {f.label}
+      </div>
+      <div className="wcp-fact-card__avatar-content__item__info__value">
+      {f.value.label}
+      </div>
+      </div>
+      </div>
+    );
 
-      return f.value.url ? (
-        <ModalContextConsumer key={`${f.label}-${f.value.label}`}>
-          {({ openModal }) => (
-            <a
-              className="wcp-fact-card__avatar-content__item-wrapper"
-              href={f.value.url}
-              onClick={e => {
-                e.preventDefault();
-                openModal(ARTICLE_SUMMARY_MODAL_ID, {
-                  articleId: slugify(f.value.label, "_")
-                });
-              }}
-            >
-              {content}
-            </a>
-          )}
-        </ModalContextConsumer>
-      ) : (
-        <div className="wcp-fact-card__avatar-content__item-wrapper">
-          content
-        </div>
-      );
-    })}
+    return f.value.url ? (
+      <ModalContextConsumer key={`${f.label}-${f.value.label}`}>
+      {({ openModal }) => (
+        <a
+        className="wcp-fact-card__avatar-content__item-wrapper"
+        href={f.value.url}
+        onClick={e => {
+          e.preventDefault();
+          openModal(ARTICLE_SUMMARY_MODAL_ID, {
+            articleId: slugify(f.value.label, "_")
+          });
+        }}
+        >
+        {content}
+        </a>
+      )}
+      </ModalContextConsumer>
+    ) : (
+      <div className="wcp-fact-card__avatar-content__item-wrapper">
+      content
+      </div>
+    );
+  })}
   </div>
 );
 
@@ -89,29 +90,29 @@ AvatarContent.propTypes = {
 const NestedContent = ({ cardData }) => {
   return (
     <div className="wcp-fact-card__nested-content">
-      {cardData.facts.map(f => {
-        const card = (
-          <Card
-            shadowSize="s"
-            title={f.value.label}
-            titleClassName="wcp-fact-card__nested-content__title"
-            coverImage={f.value.image}
-            className="wcp-fact-card__nested-content__item__card"
-            imagePosition="left"
-          />
-        );
-        return (
-          <div
-            key={`${f.label}-${f.value.label}`}
-            className="wcp-fact-card__nested-content__item"
-          >
-            <div className="wcp-fact-card__nested-content__item__label">
-              {f.label}
-            </div>
-            {f.value.url ? <a href={f.value.url}>{card}</a> : card}
-          </div>
-        );
-      })}
+    {cardData.facts.map(f => {
+      const card = (
+        <Card
+        shadowSize="s"
+        title={f.value.label}
+        titleClassName="wcp-fact-card__nested-content__title"
+        coverImage={f.value.image}
+        className="wcp-fact-card__nested-content__item__card"
+        imagePosition="left"
+        />
+      );
+      return (
+        <div
+        key={`${f.label}-${f.value.label}`}
+        className="wcp-fact-card__nested-content__item"
+        >
+        <div className="wcp-fact-card__nested-content__item__label">
+        {f.label}
+        </div>
+        {f.value.url ? <a href={f.value.url}>{card}</a> : card}
+        </div>
+      );
+    })}
     </div>
   );
 };
@@ -165,61 +166,7 @@ SimpleContent.propTypes = {
   openShareSheet: PropTypes.func.isRequired
 };
 
-const VerticalTimelineContent = ({ cardData }) => {
-  return (
-    <div className="wcp-fact-card__vertical-timeline-content">
-      {cardData.facts.map(f => {
-        const innerCardsList = f.value.innerCards.map(g => {
-          return (
-            <Card
-              shadowSize="s"
-              coverImage={g.value.image}
-              imagePosition="left"
-              title={g.label}
-              titleClassName="wcp-fact-card__vertical-timeline-content__item__card__title"
-              className="wcp-fact-card__vertical-timeline-content__item__card"
-              coverImageClassName="wcp-fact-card__vertical-timeline-content__item__card__cover-image"
-              contentClassName="wcp-fact-card__vertical-timeline-content__item__card__content"
-              children={
-                <div className="wcp-fact-card__vertical-timeline-content__item__card__description">
-                  {g.value.label}
-                </div>
-              }
-            />
-          );
-        });
-        return (
-          <div
-            key={`${f.label}-${f.value.label}`}
-            className="wcp-fact-card__vertical-timeline-content__item"
-          >
-            <h2 className="wcp-fact-card__vertical-timeline-content__item__label">
-              {f.label}
-              {f.labelHelperText ? (
-                <span className="wcp-fact-card__vertical-timeline-content__item__label-helper-text">
-                  {" "}
-                  ({f.labelHelperText})
-                </span>
-              ) : (
-                ""
-              )}
-            </h2>
-            <div className="wcp-fact-card__vertical-timeline-content__item__content">
-              {f.value.label}
-            </div>
-            <div className="wcp-fact-card__vertical-timeline-content__inner-cards-list">
-              {innerCardsList}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
 
-VerticalTimelineContent.propTypes = {
-  cardData: factCardDataPropTypes.isRequired
-};
 
 export const FactCard = ({ cardData, className }) => {
   let content = null;
@@ -243,7 +190,7 @@ export const FactCard = ({ cardData, className }) => {
       titleClassName="wcp-fact-card__title"
       contentClassName="wcp-fact-card__content"
     >
-      {content}
+    {content}
     </Card>
   );
 };
