0ba446d51d42dcac0e1645b027ab70fdb3c24b26
Ankur Sethi <contact@ankursethi.in>
2019-04-22 15:58:17 +0530

    Remove useless dependencies and files

--- cypress/integration/sample_spec.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable func-names */
-
-describe("My First Test", function() {
-  it("Visits the Kitchen Sink", function() {
-    cy.visit("http://localhost:3000");
-    cy.contains("Hello, Wikipedia!");
-  });
-});
