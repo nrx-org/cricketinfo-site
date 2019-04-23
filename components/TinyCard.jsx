f8a4bf5834853b51f2c4ce0e2adcd9ad1d4b5b31
Meet <meetbhatt23@gmail.com>
2019-04-23 18:36:21 +0530

    Fixing timeline line to be css-only

---
+++ components/TinyCard.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { imagePropTypes } from "../lib/prop_types";
+
+import { Card } from "./Card";
+
+export const TinyCard = ({
+  id,
+  children,
+  coverImage,
+  coverImageClassName,
+  title,
+  titleClassName,
+  shadowSize,
+  className,
+  contentClassName,
+  imagePosition,
+  content
+}) => {
+  return (
+    <Card
+      shadowSize={shadowSize}
+      coverImage={coverImage}
+      imagePosition={imagePosition}
+      title={title}
+      titleClassName={`wcp-tiny-card__title ${titleClassName}`}
+      className={`wcp-tiny-card ${className}`}
+      coverImageClassName={`wcp-tiny-card__cover-image ${coverImageClassName}`}
+      contentClassName={`wcp-tiny-card__content ${contentClassName}`}
+    >
+      <div className="wcp-tiny-card__description">{content}</div>
+      {children}
+    </Card>
+  );
+};
+
+TinyCard.defaultProps = {
+  id: null,
+  children: null,
+  coverImage: null,
+  coverImageClassName: "",
+  title: null,
+  titleClassName: "",
+  shadowSize: "l",
+  className: "",
+  contentClassName: "",
+  imagePosition: "top",
+  content: ""
+};
+
+TinyCard.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.node,
+  coverImage: imagePropTypes,
+  coverImageClassName: PropTypes.string,
+  title: PropTypes.string,
+  titleClassName: PropTypes.string,
+  shadowSize: PropTypes.string,
+  className: PropTypes.string,
+  contentClassName: PropTypes.string,
+  imagePosition: PropTypes.oneOf(["left", "top"]),
+  content: PropTypes.string
+};
--- components/VerticalTimelineContentSingle.jsx
+++ components/VerticalTimelineContentSingle.jsx
@@ -1,79 +1,42 @@
 import React from "react";
 import { factPropTypes } from "../lib/prop_types";
-import { Card } from "./Card";
+import { TinyCard } from "./TinyCard";
 
-export class VerticalTimelineContentSingle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.selector = React.createRef();
-    this.state = {
-      lineStyle: {
-        height: 0
-      }
-    };
-  }
-
-  componentDidMount() {
-    const rect = this.selector.current.getBoundingClientRect();
-    this.setState({ lineStyle: { height: `${rect.height}px` } });
-  }
-
-  render() {
-    const { fact } = this.props;
-    const { lineStyle } = this.state;
-    const innerCardsList = fact.value.innerCards.map(g => {
-      return (
-        <Card
-          key={`${g.label}-${g.id}`}
-          shadowSize="s"
-          coverImage={g.value.image}
-          imagePosition="left"
-          title={g.label}
-          titleClassName="wcp-fact-card__vertical-timeline-content__item__card__title"
-          className="wcp-fact-card__vertical-timeline-content__item__card"
-          coverImageClassName="wcp-fact-card__vertical-timeline-content__item__card__cover-image"
-          contentClassName="wcp-fact-card__vertical-timeline-content__item__card__content"
-        >
-          <div className="wcp-fact-card__vertical-timeline-content__item__card__description">
-            {g.value.label}
-          </div>
-        </Card>
-      );
-    });
+export const VerticalTimelineContentSingle = ({ fact }) => {
+  const innerCardsList = fact.value.innerCards.map(g => {
     return (
-      <div
-        ref={this.selector}
-        className="wcp-fact-card__vertical-timeline-content__item"
-      >
-        <h2 className="wcp-fact-card__vertical-timeline-content__item__label">
-          {fact.label}
-          {fact.labelHelperText ? (
-            <span className="wcp-fact-card__vertical-timeline-content__item__label-helper-text">
-              {" "}
-              {fact.labelHelperText}
-            </span>
-          ) : (
-            ""
-          )}
-        </h2>
-        <div className="wcp-fact-card__vertical-timeline-content__item__content">
-          {fact.value.label}
-        </div>
-        <div className="wcp-fact-card__vertical-timeline-content__inner-cards-list">
-          {innerCardsList}
-        </div>
-        <div className="wcp-fact-card__vertical-timeline-content__line-and-dot">
-          <div className="wcp-fact-card__vertical-timeline-content__dot" />
-
-          <div
-            className="wcp-fact-card__vertical-timeline-content__line"
-            style={lineStyle}
-          />
-        </div>
-      </div>
+      <TinyCard
+        key={`${g.label}-${g.id}`}
+        shadowSize="s"
+        coverImage={g.value.image}
+        imagePosition="left"
+        title={g.label}
+        content={g.value.label}
+      />
     );
-  }
-}
+  });
+  return (
+    <div className="wcp-fact-card__vertical-timeline-content__item">
+      <h2 className="wcp-fact-card__vertical-timeline-content__item__label">
+        {fact.label}
+        {fact.labelHelperText ? (
+          <span className="wcp-fact-card__vertical-timeline-content__item__label-helper-text">
+            {" "}
+            ({fact.labelHelperText})
+          </span>
+        ) : (
+          ""
+        )}
+      </h2>
+      <div className="wcp-fact-card__vertical-timeline-content__item__content">
+        {fact.value.label}
+      </div>
+      <div className="wcp-fact-card__vertical-timeline-content__inner-cards-list">
+        {innerCardsList}
+      </div>
+    </div>
+  );
+};
 
 VerticalTimelineContentSingle.propTypes = {
   fact: factPropTypes
