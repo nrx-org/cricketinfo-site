544540f5b6aeb44bc6384e27f0accfe2f96fc2df
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-05-03 18:38:26 +0530

    Add basic JSON object

--- bin/make_article.js
+++ bin/make_article.js
@@ -20,7 +20,138 @@ const records = parse(input, {
 let article;
 
 records.forEach(record => {
-
+  // Putting together the completed JS object
+  article = {
+    title: record["Name of personality"],
+    coverImage: {
+      url: record["Header image"],
+      altText: record["Name of personality"] //TODO: change this to image title
+    },
+    summary: record["Short description of personality"],
+    sections: [
+      {
+        title: "About",
+        cardType: "table",
+        facts: [
+          { label: "Full Name", value: record["Birth Name"] },
+          { label: "Born on", value: record["Born on"] }, //TODO: format the dates on this line
+          {
+            label: "Birthplace",
+            value: {
+              label: record["Birthplace"],
+              url: record["Wikipedia article link of the Birth place"]
+            }
+          },
+          { label: "Height", value: record["Height"] },
+          { label: "Other Names", value: record["Nicknames/Other names"] },
+          //   { label: "Age", value: record["Age"] },
+          //   { label: "Residence", value: record["Residence"] },
+          {
+            label: "Nationality",
+            value: {
+              label: record["Nationality"],
+              url: record["Wikipedia article link of the Nationality"]
+            }
+          },
+          { label: "Role", value: record["Fielding position/Role"] }
+        ]
+      },
+      {
+        title: "Family",
+        cardType: "avatar",
+        label: "",
+        facts: [
+          {
+            label: record["Relationship with personality"],
+            value: {
+              label: record["Relation 1"],
+              image: {
+                url: record["Link to the image of relation 1"],
+                altText: "text goes here" //TODO: Replace
+              }
+            }
+          }
+        ]
+      },
+      {
+        title: "Phase(s)",
+        cardType: "vertical_timeline",
+        facts: [
+          {
+            label: "Year of Birth",
+            id: 0,
+            note: null,
+            value: {
+              label: record["Phase 1 - Single line description"],
+              facts: [
+                {
+                  label: "Place of Birth",
+                  id: 0,
+                  value: {
+                    label: record["Phase 1 - Place of birth"],
+                    url: record["Phase 1 - Link to the place of birth "],
+                    image: {
+                      url: record["Phase 1- Image related to the place"],
+                      altText: "Kitten mata ki jai"
+                    }
+                  }
+                }
+              ]
+            }
+          },
+          {
+            label: "Debut Year",
+            id: 3,
+            note: record["Phase 2 - First Class Debut Year"],
+            value: {
+              label: record["Phase 2 - Single line description"],
+              facts: [
+                {
+                  label: record["Phase 2 - Name of the debut trophy"],
+                  id: 0,
+                  value: {
+                    label: "",
+                    url: record["Phase 2 - Link to the Trophy article"],
+                    image: {
+                      url: "https://placekitten.com/200/200",
+                      altText: "Kitten mata ki jai"
+                    }
+                  }
+                }
+              ]
+            }
+          },
+          {
+            label: record["National Team Debut"],
+            note:
+              record["Phase 3 - National team debut year in cricket form 1"],
+            id: 1,
+            value: {
+              label: record["Phase 3 - Opposition team"],
+              facts: [
+                {
+                  label: "",
+                  id: 0,
+                  value: {
+                    label: record["Phase 3 - Single line description"],
+                    url:
+                      record[
+                        "Phase 3 - Link to the opposition team's wikipedia page"
+                      ],
+                    image: {
+                      url: "https://placekitten.com/200/200",
+                      altText: "Kitten mata ki jai"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }, 
+      
+    ]
+  };
 
   const sluggedTitle = slugify(article.title, "_").toLowerCase(); //TODO: remove slugify
 
