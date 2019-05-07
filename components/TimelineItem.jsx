fd9d9006f3f7c6480c58aa6f1a9c96371d283407
Meet <meetbhatt23@gmail.com>
2019-05-07 14:33:30 +0530

    Fixes for horizontal timeline styles, adding full bleed support for horizontal and vertical timelines

--- components/TimelineItem.jsx
+++ components/TimelineItem.jsx
@@ -4,21 +4,23 @@ import { TinyCardCarousel } from "./TinyCardCarousel";
 
 export const TimelineItem = ({ fact }) => (
   <div className="wcp-timeline-item">
-    <h2 className="wcp-timeline-item__label">
-      {fact.label}
-      {fact.note ? (
-        <span className="wcp-timeline-item__label-helper-text">
-          {" "}
-          {"("}
-          {fact.note}
-          {")"}
-        </span>
-      ) : (
-        ""
-      )}
-    </h2>
+    <div className="wcp-timeline-item__text-content">
+      <h2 className="wcp-timeline-item__label">
+        {fact.label}
+        {fact.note ? (
+          <span className="wcp-timeline-item__label-helper-text">
+            {" "}
+            {"("}
+            {fact.note}
+            {")"}
+          </span>
+        ) : (
+          ""
+        )}
+      </h2>
 
-    <div className="wcp-timeline-item__content">{fact.value.label}</div>
+      <div className="wcp-timeline-item__content">{fact.value.label}</div>
+    </div>
     <div className="wcp-timeline-item__line-and-dot">
       <div className="wcp-timeline-item__line-and-dot__dot" />
       <div className="wcp-timeline-item__line-and-dot__line" />
