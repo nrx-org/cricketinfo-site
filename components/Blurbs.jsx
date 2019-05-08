ff5afa0adc8eb901e6de28e80b297e0a0e9202bb
Meet <meetbhatt23@gmail.com>
2019-05-08 16:34:28 +0530

    Blurbs section

---
+++ components/Blurbs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { factCardDataPropTypes } from "../lib/prop_types";
+import { SectionTitle } from "./SectionTitle";
+import { TinyCardCarousel } from "./TinyCardCarousel";
+
+export const Blurbs = ({ cardData }) => {
+  return (
+    <section>
+      <SectionTitle>{cardData.title}</SectionTitle>
+
+      <div className="wcp-blurbs">
+        {cardData.facts.map(fact => {
+          return (
+            <div className="wcp-blurbs__item" key={fact.id}>
+              <div className="wcp-blurbs__item__text-content">
+                <h2 className="wcp-blurbs__item__label">{fact.label}</h2>
+              </div>
+              <TinyCardCarousel cards={fact.value.facts} />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+Blurbs.propTypes = {
+  cardData: factCardDataPropTypes.isRequired
+};
