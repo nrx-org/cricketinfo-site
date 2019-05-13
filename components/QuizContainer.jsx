f0f8b684b8ef532dc54c0a17cffa26a866a9b302
Ankur Sethi <contact@ankursethi.in>
2019-05-13 18:47:28 +0530

    Save the user's answers to quiz questions in localStorage

--- components/QuizContainer.jsx
+++ components/QuizContainer.jsx
@@ -1,15 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { LargeSectionTitle } from "./LargeSectionTitle";
 import { QuizQuestion } from "./QuizQuestion";
 
-export const QuizContainer = ({ questions }) => (
-  <section>
-    <LargeSectionTitle>Do you know?</LargeSectionTitle>
-    {questions.map(q => (
-      <QuizQuestion question={q} userAnswerIndex={0} />
-    ))}
-  </section>
-);
+const LOCALSTORAGE_KEY = "wcpQuizContainerData";
+
+export class QuizContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setUserAnswerIndex = this.setUserAnswerIndex.bind(this);
+  }
+
+  static getUserAnswerIndex(questionId) {
+    if (!process.browser) {
+      return -1;
+    }
+
+    const quizContainerDataString = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (!quizContainerDataString) {
+      return -1;
+    }
+
+    const quizContainerData = JSON.parse(quizContainerDataString);
+
+    if (typeof quizContainerData[questionId] === "undefined") {
+      return -1;
+    }
+
+    return quizContainerData[questionId];
+  }
+
+  setUserAnswerIndex(questionId, userAnswerIndex) {
+    if (!process.browser) {
+      return;
+    }
+
+    const quizContainerDataString = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    let quizContainerData = {};
+
+    if (quizContainerDataString) {
+      quizContainerData = JSON.parse(quizContainerDataString);
+    }
+
+    quizContainerData[questionId] = userAnswerIndex;
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(quizContainerData));
+
+    this.forceUpdate();
+  }
+
+  render() {
+    let { questions } = this.props;
+
+    return (
+      <section>
+        <LargeSectionTitle>Do you know?</LargeSectionTitle>
+        {questions.map(q => (
+          <QuizQuestion
+            question={q}
+            userAnswerIndex={QuizContainer.getUserAnswerIndex(q.id)}
+            setUserAnswerIndex={index => this.setUserAnswerIndex(q.id, index)}
+          />
+        ))}
+      </section>
+    );
+  }
+}
 
 QuizContainer.propTypes = {};
--- components/QuizQuestion.jsx
+++ components/QuizQuestion.jsx
@@ -1,45 +1,90 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { LanguageContextConsumer } from "../language_context";
+import { TinyCard } from "./TinyCard";
 
-export const QuizQuestion = ({ question, userAnswerIndex }) => {
+const CORRECT_ANSWER_MESSAGE = {
+  en: "Correct answer! Learn more:",
+  hi: "सही जवाब! और जानें:",
+  ta: "TODO"
+};
+
+const INCORRECT_ANSWER_MESSAGE = (lang, answerIndex) => {
+  const answerLetter = ["A", "B"][answerIndex];
+
+  return {
+    en: `Wrong answer! The correct answer is option ${answerLetter}. Learn more:`,
+    hi: `गलत जवाब! सही जवाब है विकल्प ${answerLetter}। और जानें:`,
+    ta: "TODO"
+  }[lang];
+};
+
+export const QuizQuestion = ({
+  question,
+  userAnswerIndex,
+  setUserAnswerIndex
+}) => {
   const isAnswered = userAnswerIndex !== -1;
   const isAnsweredCorrectly = userAnswerIndex === question.answerIndex;
 
   return (
-    <div className="wcp-quiz-question">
-      <p>{question.label}</p>
-      <ul className="wcp-quiz-question__answers">
-        {question.options.map((o, index) => {
-          let className = "wcp-quiz-question__answers__answer-wrapper";
+    <LanguageContextConsumer>
+      {lang => (
+        <div className="wcp-quiz-question">
+          <p>{question.label}</p>
+          <ul className="wcp-quiz-question__answers">
+            {question.options.map((o, index) => {
+              let className = "wcp-quiz-question__answers__answer-wrapper";
 
-          className = `${className} ${
-            isAnswered && index !== userAnswerIndex
-              ? "wcp-quiz-question__answers__answer-wrapper--inactive"
-              : ""
-          }`;
+              className = `${className} ${
+                isAnswered && index !== userAnswerIndex
+                  ? "wcp-quiz-question__answers__answer-wrapper--inactive"
+                  : ""
+              }`;
 
-          className = `${className} ${
-            isAnsweredCorrectly && index === question.answerIndex
-              ? "wcp-quiz-question__answers__answer-wrapper--correct"
-              : ""
-          }`;
+              className = `${className} ${
+                isAnsweredCorrectly && index === question.answerIndex
+                  ? "wcp-quiz-question__answers__answer-wrapper--correct"
+                  : ""
+              }`;
 
-          className = `${className} ${
-            !isAnsweredCorrectly && index === userAnswerIndex
-              ? "wcp-quiz-question__answers__answer-wrapper--incorrect"
-              : ""
-          }`;
+              className = `${className} ${
+                !isAnsweredCorrectly && index === userAnswerIndex
+                  ? "wcp-quiz-question__answers__answer-wrapper--incorrect"
+                  : ""
+              }`;
 
-          return (
-            <li className={className}>
-              <span className="wcp-quiz-question__answers__answer">
-                {o.label}
-              </span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+              return (
+                <li
+                  className={className}
+                  onClick={() => setUserAnswerIndex(index)}
+                >
+                  <span className="wcp-quiz-question__answers__answer">
+                    {o.label}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+
+          {isAnswered && isAnsweredCorrectly ? (
+            <p>{CORRECT_ANSWER_MESSAGE[lang]}</p>
+          ) : null}
+          {isAnswered && !isAnsweredCorrectly ? (
+            <p>{INCORRECT_ANSWER_MESSAGE(lang, question.answerIndex)}</p>
+          ) : null}
+          {isAnswered ? (
+            <TinyCard
+              title={question.relatedArticle.label}
+              coverImage={question.relatedArticle.value.image}
+              href={question.relatedArticle.value.url}
+            >
+              {question.relatedArticle.value.label}
+            </TinyCard>
+          ) : null}
+        </div>
+      )}
+    </LanguageContextConsumer>
   );
 };
 
