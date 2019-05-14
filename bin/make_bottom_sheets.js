fda5a4d1d508c558ffb798269e4b20f2f88b644b
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-05-14 20:31:38 +0530

    Get and store images for the bottom sheet articles

--- bin/make_bottom_sheets.js
+++ bin/make_bottom_sheets.js
@@ -1,7 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable dot-notation */
 const fs = require("fs");
 const parse = require("csv-parse/lib/sync");
-// eslint-disable-next-line import/no-extraneous-dependencies
+const fetch = require("isomorphic-fetch");
 const slugify = require("slugify");
 
 const pathToParsedFile = "./csv/bottom_sheets.csv";
@@ -18,18 +19,42 @@ const records = parse(input, {
 
 let bottomSheet;
 
-records.forEach(element => {
+records.forEach(async element => {
   bottomSheet = {
     id: element["su"],
-    title: element["Title"],
+    title: element["Title"]
+  };
+
+  const getSluggedTitle = slugify(bottomSheet.title, "_").toLowerCase(); // TODO: remove slugify
+  
+  const getFileNameFromURL = url => {
+    if (!url) return null;
+    return url.match(/File:(.*)/)[1];
+  };
+  
+  const getImageForArticle = async url => {
+    const image = await fetch(url);
+    const imageName = getFileNameFromURL(url);
+    const imageDirectory = `./static/images/${getSluggedTitle}`;
+    const imagePath = `./static/images/${getSluggedTitle}/${imageName}`;
+    if (!fs.existsSync(imageDirectory)) {
+      fs.mkdir(imageDirectory, err => {
+        if (err) throw err;
+      });
+    }
+    const imageFile = fs.createWriteStream(imagePath);
+    image.body.pipe(imageFile);
+    return imagePath.substring(1);
+  };
+
+  bottomSheet = {
+    ...bottomSheet,
     coverImg: {
-      url: element["Image Link"],
+      url: `${await getImageForArticle(element["Image Link"])}`,
       altText: element["Image caption"]
     },
     summary: element["Short description"]
   };
-
-  const getSluggedTitle = slugify(bottomSheet.title, "_").toLowerCase(); // TODO: remove slugify
 
   fs.writeFile(
     `./csv/${getSluggedTitle}.json`,
