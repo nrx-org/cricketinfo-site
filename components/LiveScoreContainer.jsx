47ec7eebc567acd12f1abcab311d5872c68a4c89
Ankur Sethi <contact@ankursethi.in>
2019-05-22 18:42:22 +0530

    Account for different match statuses, remove test data, and fix styles

--- components/LiveScoreContainer.jsx
+++ components/LiveScoreContainer.jsx
@@ -4,7 +4,7 @@ import { getLiveScore } from "../lib/live_score";
 
 const formatScore = team => {
   if (!team.score) {
-    return null;
+    return "-";
   }
 
   if (team.score.runs === 0 && team.score.wickets === 0) {
@@ -13,6 +13,9 @@ const formatScore = team => {
 
   return `${team.score.runs}/${team.score.wickets}`;
 };
+
+const MATCH_STATUS_STARTED = "started";
+const MATCH_STATUS_COMPLETED = "completed";
 
 export class LiveScoreContainer extends React.Component {
   constructor(props) {
@@ -48,13 +51,19 @@ export class LiveScoreContainer extends React.Component {
 
     return (
       <div className="wcp-live-score-container">
-        <div className="wcp-live-score-container__title">
-          <div className="wcp-live-score-container__title__dot" />
-          <span className="wcp-live-score-container__title__text">Live</span>
-        </div>
-
-        <div className="wcp-live-score-container__score-table-container">
-          {score.matches.map(match => (
+        {score.matches.map(match => (
+          <div className="wcp-live-score-container__score-table-container">
+            <div className="wcp-live-score-container__title">
+              {match.status === MATCH_STATUS_STARTED ? (
+                <div className="wcp-live-score-container__title__dot" />
+              ) : null}
+              <span className="wcp-live-score-container__title__text">
+                {match.status === MATCH_STATUS_STARTED ? "LIVE" : ""}
+                {match.status === MATCH_STATUS_COMPLETED && match.resultMessage
+                  ? match.resultMessage
+                  : ""}
+              </span>
+            </div>
             <table className="wcp-live-score-container__score-table">
               <tr>
                 <th className="wcp-font-family-heading">
@@ -83,8 +92,8 @@ export class LiveScoreContainer extends React.Component {
                 <td>{formatScore(match.teamB)}</td>
               </tr>
             </table>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     );
   }
