b17f2e4ebcbd97a87f9e1be97e91f0b684e3097e
Meet <meetbhatt23@gmail.com>
2019-05-23 14:53:12 +0530

    Matomo Setup

---
+++ pages/_document.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+
+import {
+  MATOMO_SCRIPT_URL,
+  MATOMO_TRACKER_URL,
+  MATOMO_SITEID
+} from "../lib/matomo";
+
+export default class extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <script async src={MATOMO_SCRIPT_URL} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            var _paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function(){
+              _paq.push(['setTrackerUrl', "${MATOMO_TRACKER_URL}"]);
+              _paq.push(['setSiteId', "${MATOMO_SITEID}"]);
+            })()  
+          `
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
