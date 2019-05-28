b1aa7114a6cecb4313c057deb6ac0ba219ba82be
Ankur Sethi <contact@ankursethi.in>
2019-05-28 09:24:50 +0530

    Add function to get article details given an ID, and use it for phases

--- content_parser/lib.js
+++ content_parser/lib.js
@@ -1,10 +1,62 @@
-module.exports.findIdMapEntryById = (idMap, id) => {
+const fs = require("fs");
+const url = require("url");
+const parse = require("csv-parse/lib/sync");
+
+const isUrlValid = u => url.parse(u).protocol !== null;
+
+const findIdMapEntryById = (idMap, id) => {
   return idMap.find(entry => entry.id === id);
 };
 
-module.exports.getSluggedTitle = str => str.replace(/\s+/g, "_").toLowerCase();
+const getSluggedTitle = str => str.replace(/\s+/g, "_").toLowerCase();
 
-module.exports.makeArticleUrl = (lang, slug) => `/read/${lang}/${slug}`;
+const makeArticleUrl = (lang, slug) => `/read/${lang}/${slug}`;
 
-module.exports.makeContentUrl = (lang, slug) =>
-  `/static/content/${lang}/${slug}.json`;
+const makeContentUrl = (lang, slug) => `/static/content/${lang}/${slug}.json`;
+
+const getCardInfoFromId = (idMap, id, lang) => {
+  if (!id.trim()) {
+    return null;
+  }
+
+  const referencedCardEntry = findIdMapEntryById(idMap, id);
+
+  if (!referencedCardEntry) {
+    return null;
+  }
+
+  const referencedCardSourceCsvFile = fs.readFileSync(
+    referencedCardEntry.sourceCsvFile[lang],
+    "utf8"
+  );
+  const records = parse(referencedCardSourceCsvFile, {
+    columns: true,
+    delimiter: ","
+  });
+  const record = records.find(r => r[referencedCardEntry.idFieldName] === id);
+
+  if (!record) {
+    return null;
+  }
+
+  return {
+    title: record[referencedCardEntry.titleFieldName],
+    summary: record[referencedCardEntry.descriptionFieldName],
+    url: makeArticleUrl(lang, getSluggedTitle(referencedCardEntry.title.en)),
+    contentUrl: makeContentUrl(
+      lang,
+      getSluggedTitle(referencedCardEntry.title.en)
+    ),
+    imageUrl: isUrlValid(record[referencedCardEntry.imageFieldName])
+      ? record[referencedCardEntry.imageFieldName]
+      : null
+  };
+};
+
+module.exports = {
+  findIdMapEntryById,
+  getSluggedTitle,
+  makeArticleUrl,
+  makeContentUrl,
+  getCardInfoFromId
+};
