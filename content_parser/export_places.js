3659a5fd91f7a09d1b5192af692f7ccdd25c898c
Ankur Sethi <contact@ankursethi.in>
2019-05-28 19:42:03 +0530

    Add about table, history, government, sports to places articles

--- content_parser/export_places.js
+++ content_parser/export_places.js
@@ -3,7 +3,8 @@ const parse = require("csv-parse/lib/sync");
 const {
   findIdMapEntryById,
   getSluggedTitle,
-  downloadImageAndFillAttributions
+  downloadImageAndFillAttributions,
+  getCardInfoFromId
 } = require("./lib");
 const idMap = require("../static/content/article_ids.json");
 
@@ -18,6 +19,64 @@ const TITLE_KEY = "Name of the place";
 const SUMMARY_KEY = "Short summary description of the place";
 const WIKIPEDIA_URL_KEY = "Wikipedia Link to Place";
 const COVER_IMAGE_KEY = "Header image";
+const IMPORTANT_DAY_1_KEY = "Important day 1";
+const IMPORTANT_DAY_1_DATE_KEY = "Important day 1 date";
+const IMPORTANT_DAY_2_KEY = "Important day 2";
+const IMPORTANT_DAY_2_DATE_KEY = "Important day 2 date";
+const IMPORTANT_DAY_3_KEY = "Important day 3";
+const IMPORTANT_DAY_3_DATE_KEY = "Important day 3 date";
+const TOTAL_AREA_KEY = "Total Area of country (km squared)";
+const POPULATION_KEY = "Population";
+const OTHER_NAMES_KEY = "Other names";
+const CAPITAL_CITY_KEY = "Capital city";
+const LARGEST_CITY_KEY = "Largest city";
+const TIMEZONE_KEY = "Timezone";
+
+const HISTORY = [
+  {
+    TITLE_KEY: "History Phase 1 Title",
+    TIME_PERIOD_KEY: "History Phase 1 Time period",
+    DESCRIPTION_KEY: "History Phase 1 Small description"
+  },
+  {
+    TITLE_KEY: "History Phase 2 Title",
+    TIME_PERIOD_KEY: "History Phase 2 Time period",
+    DESCRIPTION_KEY: "History Phase 2  Small description"
+  },
+  {
+    TITLE_KEY: "History Phase 3 Title",
+    TIME_PERIOD_KEY: "History Phase 3 Time period",
+    DESCRIPTION_KEY: "History Phase 3  Small description"
+  },
+  {
+    TITLE_KEY: "History Phase 4 Title",
+    TIME_PERIOD_KEY: "History Phase 4 Time period",
+    DESCRIPTION_KEY: "History Phase 4  Small description"
+  }
+];
+
+const GOVERNMENT_KEY = "Government";
+
+const GOVERNMENT_PEOPLE = [
+  {
+    NAME_KEY: "Prime minister name",
+    IMAGE_KEY: "Prime minister image link"
+  },
+  {
+    NAME_KEY: "President",
+    IMAGE_KEY: "President image link"
+  },
+  {
+    NAME_KEY: "Chief Justice name",
+    IMAGE_KEY: "Chief Justice image link"
+  }
+];
+
+const IN_SPORTS = [
+  { KEY: "Team ID Code", isPlayer: false },
+  { key: "Player 1 ID Code", isPlayer: true },
+  { key: "Player 2 ID Code", isPlayer: true }
+];
 
 module.exports.exportPlaces = () => {
   Object.keys(csvExports).forEach(lang => {
@@ -55,6 +114,175 @@ module.exports.exportPlaces = () => {
       }));
 
       place.sections = [];
+
+      // About table.
+      const aboutTableFacts = [
+        record[IMPORTANT_DAY_1_KEY] && {
+          label: record[IMPORTANT_DAY_1_KEY],
+          value: {
+            label: record[IMPORTANT_DAY_1_DATE_KEY]
+          }
+        },
+        record[IMPORTANT_DAY_2_KEY] && {
+          label: record[IMPORTANT_DAY_2_KEY],
+          value: {
+            label: record[IMPORTANT_DAY_2_DATE_KEY]
+          }
+        },
+        record[IMPORTANT_DAY_3_KEY] && {
+          label: record[IMPORTANT_DAY_3_KEY],
+          value: {
+            label: record[IMPORTANT_DAY_3_DATE_KEY]
+          }
+        },
+        record[TOTAL_AREA_KEY] && {
+          label: TOTAL_AREA_KEY,
+          value: {
+            label: record[TOTAL_AREA_KEY]
+          }
+        },
+        record[POPULATION_KEY] && {
+          label: POPULATION_KEY,
+          value: {
+            label: record[POPULATION_KEY]
+          }
+        },
+        record[OTHER_NAMES_KEY] && {
+          label: OTHER_NAMES_KEY,
+          value: {
+            label: record[OTHER_NAMES_KEY]
+          }
+        },
+        record[CAPITAL_CITY_KEY] && {
+          label: CAPITAL_CITY_KEY,
+          value: {
+            label: record[CAPITAL_CITY_KEY]
+          }
+        },
+        record[LARGEST_CITY_KEY] && {
+          label: LARGEST_CITY_KEY,
+          value: {
+            label: record[LARGEST_CITY_KEY]
+          }
+        },
+        record[TIMEZONE_KEY] && {
+          label: TIMEZONE_KEY,
+          value: {
+            label: record[TIMEZONE_KEY]
+          }
+        }
+      ].filter(f => !!f);
+
+      if (aboutTableFacts.length > 0) {
+        place.sections.push({
+          title: "About",
+          cardType: "table",
+          facts: aboutTableFacts
+        });
+      }
+
+      // History.
+      const historySection = {
+        title: "History",
+        cardType: "vertical_timeline",
+        facts: HISTORY.map(historyItem => {
+          if (!record[historyItem.TITLE_KEY]) {
+            return null;
+          }
+
+          return {
+            label: record[historyItem.TITLE_KEY],
+            id: getSluggedTitle(historyItem.TITLE_KEY),
+            note: record[historyItem.TIME_PERIOD_KEY],
+            value: {
+              label: record[historyItem.DESCRIPTION_KEY]
+            }
+          };
+        }).filter(f => !!f)
+      };
+
+      if (historySection.facts.length > 0) {
+        place.sections.push(historySection);
+      }
+
+      // Government.
+      let governmentFacts = await Promise.all(
+        GOVERNMENT_PEOPLE.map(async person => {
+          if (!record[person.NAME_KEY]) {
+            return null;
+          }
+
+          return {
+            label: person.NAME_KEY,
+            value: {
+              label: record[person.NAME_KEY],
+              image: await downloadImageAndFillAttributions(
+                {
+                  url: record[person.IMAGE_KEY],
+                  altText: `Picture of ${record[person.NAME_KEY]}`
+                },
+                englishSlug
+              )
+            }
+          };
+        })
+      );
+
+      governmentFacts = governmentFacts.filter(f => !!f);
+
+      if (governmentFacts.length > 0) {
+        place.sections.push({
+          title: "Government",
+          cardType: "avatar",
+          label: record[GOVERNMENT_KEY],
+          facts: governmentFacts
+        });
+      }
+
+      // In sports.
+      let inSportsFacts = await Promise.all(
+        IN_SPORTS.map(async inSport => {
+          if (!record[inSport.KEY]) {
+            return null;
+          }
+
+          const card = getCardInfoFromId(idMap, record[inSport.KEY], lang);
+
+          if (!card) {
+            return null;
+          }
+
+          return {
+            label: inSport.isPlayer ? "Player" : "Team",
+            value: {
+              label: card.title,
+              url: card.url,
+              contentUrl: card.contentUrl,
+              image: await downloadImageAndFillAttributions({
+                url: card.imageUrl,
+                altText: `Picture of ${card.title}`
+              })
+            }
+          };
+        })
+      );
+
+      inSportsFacts = inSportsFacts.filter(f => !!f);
+
+      if (inSportsFacts.length > 0) {
+        place.sections.push({
+          title: "In Sports",
+          cardType: "avatar",
+          label: "",
+          facts: inSportsFacts
+        });
+      }
+
+      // Geography.
+
+      // Culture.
+
+      // Demographics.
 
       fs.writeFileSync(
         `./static/content/${lang}/${englishSlug}.json`,
--- content_parser/index.js
+++ content_parser/index.js
@@ -4,6 +4,6 @@ const { exportHome } = require("./export_home");
 const { exportPlaces } = require("./export_places");
 
 exportIdAndUrlMaps();
-exportPersonalities();
-exportHome();
+// exportPersonalities();
+// exportHome();
 exportPlaces();
