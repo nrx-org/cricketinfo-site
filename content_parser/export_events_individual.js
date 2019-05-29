8d372e7cdf27f371bae5d1587b5056c56263c188
Ankur Sethi <contact@ankursethi.in>
2019-05-29 08:26:35 +0530

    Add basic structure for individual events parser

---
+++ content_parser/export_events_individual.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const parse = require("csv-parse/lib/sync");
+const {
+  findIdMapEntryById,
+  getSluggedTitle,
+  downloadImageAndFillAttributions,
+  getCardInfoFromId
+} = require("./lib");
+const idMap = require("../static/content/article_ids.json");
+
+const csvExports = {
+  en: { path: "./csv/events_individual.en.csv" },
+  hi: { path: "./csv/events_individual.hi.csv" },
+  ta: { path: "./csv/events_individual.ta.csv" }
+};
+
+const ID_KEY = "Article Link ID";
+const TITLE_KEY = "Name of tournament";
+const SUMMARY_KEY = "Short summary of the article";
+const WIKIPEDIA_URL_KEY = "Wikipedia link of the Tournament";
+const COVER_IMAGE_KEY = "Header  image ";
+
+module.exports.exportEventsIndividual = () => {
+  Object.keys(csvExports).forEach(lang => {
+    const fileContent = fs.readFileSync(csvExports[lang].path, "utf8");
+    const records = parse(fileContent, { columns: true, delimiter: "," });
+
+    records.forEach(async record => {
+      const idTitleMap = findIdMapEntryById(idMap, record[ID_KEY]);
+      const englishSlug = getSluggedTitle(idTitleMap.title.en);
+      const event = {};
+
+      event.title = record[TITLE_KEY];
+      if (!event.title) {
+        return;
+      }
+
+      event.summary = record[SUMMARY_KEY];
+      event.wikipediaUrl = record[WIKIPEDIA_URL_KEY];
+      event.coverImage = await downloadImageAndFillAttributions(
+        {
+          url: record[COVER_IMAGE_KEY],
+          altText: `Image of ${event.title}`
+        },
+        englishSlug
+      );
+
+      const translationLanguages = Object.keys(csvExports).filter(
+        l => l !== lang
+      );
+
+      event.translations = translationLanguages.map(tl => ({
+        lang: tl,
+        title: idTitleMap.title[tl],
+        url: `/read/${tl}/${englishSlug}`
+      }));
+
+      event.sections = [];
+
+      fs.writeFileSync(
+        `./static/content/${lang}/${englishSlug}.json`,
+        JSON.stringify(event, null, 2)
+      );
+    });
+  });
+};
--- content_parser/index.js
+++ content_parser/index.js
@@ -2,6 +2,7 @@ const { exportIdAndUrlMaps } = require("./export_id_url_maps");
 const { exportPersonalities } = require("./export_personalities");
 const { exportHome } = require("./export_home");
 const { exportPlaces } = require("./export_places");
+const { exportEventsIndividual } = require("./export_events_individual");
 
 exportIdAndUrlMaps();
 exportPersonalities();
