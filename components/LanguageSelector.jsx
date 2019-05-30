46fb21c1dd9ef44f3b1d9f6b8e33b61116879f94
Ankur Sethi <contact@ankursethi.in>
2019-05-30 10:17:34 +0530

    Add UI string translations to the language selector

--- components/LanguageSelector.jsx
+++ components/LanguageSelector.jsx
@@ -1,49 +1,37 @@
 import React from "react";
 
 import { CoverCard } from "./CoverCard";
-import { translationsPropTypes, imagePropTypes } from "../lib/prop_types";
-import { LanguageContext } from "../language_context";
+import { imagePropTypes, translationsPropTypes } from "../lib/prop_types";
+import { LanguageContextConsumer } from "../language_context";
+import { articleUiStrings } from "../lib/ui_strings";
 
-const LINK_TEXT = {
-  hi: "हिंदी में पढ़े",
-  en: "Read in English",
-  pa: "ਪੰਜਾਬੀ ਵਿੱਚ ਪੜ੍ਹਿਆ"
+export const LanguageSelector = ({ translations, coverImage }) => {
+  return (
+    <LanguageContextConsumer>
+      {lang => (
+        <section className="wcp-language-selector">
+          <h2 className="wcp-language-selector__title">
+            {articleUiStrings.readInOtherLanguages[lang]}
+          </h2>
+          <div className="wcp-language-selector__cards">
+            {translations.map(t => {
+              return (
+                <CoverCard
+                  key={t.title}
+                  url={t.url}
+                  title={t.title}
+                  coverImage={coverImage}
+                  coverImageClassName="wcp-language-selector__cover-image"
+                  linkText={articleUiStrings.readInLanguage[t.lang]}
+                />
+              );
+            })}
+          </div>
+        </section>
+      )}
+    </LanguageContextConsumer>
+  );
 };
-
-const TITLE_TEXT = {
-  hi: "अन्य भाषाओं में संबंधित लेख",
-  en: "Read in other language(s)",
-  pa: ""
-};
-
-/* eslint-disable react/prefer-stateless-function */
-export class LanguageSelector extends React.Component {
-  render() {
-    const { translations, coverImage } = this.props;
-    const lang = this.context;
-    const cards = translations.map(t => {
-      return (
-        <CoverCard
-          key={t.title}
-          url={t.url}
-          title={t.title}
-          coverImage={coverImage}
-          coverImageClassName="wcp-language-selector__cover-image"
-          linkText={LINK_TEXT[t.lang]}
-        />
-      );
-    });
-    return (
-      <section className="wcp-language-selector">
-        <h2 className="wcp-language-selector__title">{TITLE_TEXT[lang]}</h2>
-        <div className="wcp-language-selector__cards">{cards}</div>
-      </section>
-    );
-  }
-}
-/* eslint-enable react/prefer-stateless-function */
-
-LanguageSelector.contextType = LanguageContext;
 
 LanguageSelector.propTypes = {
   translations: translationsPropTypes.isRequired,
